50e88f33404174905215b5073b60d1f4406410d4
Aldair0308 <al222111405@gmail.com>
2024-07-13 16:57:33 -0600

    error 419

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -2,15 +2,22 @@
 <html>
 <head>
     <title>419 - Sesión Expirada</title>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 </head>
 <body>
-    <div>
-        <p>La sesión ha expirado. Serás redirigido a la página de inicio en unos segundos.</p>
-        <script>
-            setTimeout(function() {
-                window.location.href = "{{ route('home') }}";
-            }, 5000); // Redirigir después de 5 segundos (5000 milisegundos)
-        </script>
-    </div>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            Swal.fire({
+                icon: 'warning',
+                title: '¡La sesión ha expirado!',
+                text: 'Serás redirigido a la página de inicio en unos segundos.',
+                timer: 5000, // Tiempo en milisegundos (5 segundos)
+                timerProgressBar: true,
+                willClose: () => {
+                    window.location.href = "{{ route('welcome') }}"; // Redirige a la página de bienvenida
+                }
+            });
+        });
+    </script>
 </body>
 </html>
