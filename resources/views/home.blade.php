da98fd9546b4e4d24526cd9b44b17a582b7ddda6
Aldair0308 <al222111405@gmail.com>
2024-08-04 01:55:04 -0600

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,29 @@
+$<!doctype html>
+<html lang="en">
+    <head>
+        <title>Casa</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
 
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+
+    <body>
+        <header>
+            <!-- place navbar here -->
+        </header>
+        <main>
+            
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -6,20 +31,36 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+
                     <div class="card">
                         <div class="card-text">
                             <a href="{{ route('mesas.index') }}" class="btn btn-success">Ir a mesas</a>
+                            <a href="{{ route('login') }}" class="btn btn-danger">cerrar sesion</a>
                         </div>
                     </div>
 
-                    {{ __('You are logged in!') }}
-                </div>
             </div>
         </div>
     </div>
 </div>
+        </main>
+        <footer>
+            <!-- place footer here -->
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
+
+
+
