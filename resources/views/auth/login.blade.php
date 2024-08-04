9020d3fcf79f5be1af810f5b9880ade3680eedbd
Aldair0308 <al222111405@gmail.com>
2024-08-04 00:57:45 -0600

    login try three

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,12 @@
     <!--Bootstrap 4 CDN-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     
+    <!--Fontawesome CDN-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+    <!--Custom styles-->
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('css/login-styles.css') }}">
+
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
@@ -14,26 +20,35 @@
         <div class="card">
             <div class="card-header">
                 <h3>Inicia Sesión</h3>
+                <div class="d-flex justify-content-end social_icon">
+                    <span><i class="fab fa-facebook-square"></i></span>
+                    <span><i class="fab fa-google-plus-square"></i></span>
+                    <span><i class="fab fa-twitter-square"></i></span>
+                </div>
             </div>
             <div class="card-body">
                 <form id="login-form">
-                    @csrf
                     <div class="input-group form-group">
                         <div class="input-group-prepend">
                             <span class="input-group-text"><i class="fas fa-user"></i></span>
                         </div>
-                        <input type="text" class="form-control" placeholder="Correo electrónico" id="email" name="email" required>
+                        <input type="text" class="form-control" placeholder="Correo electrónico" id="email" name="email">
                     </div>
                     <div class="input-group form-group">
                         <div class="input-group-prepend">
                             <span class="input-group-text"><i class="fas fa-key"></i></span>
                         </div>
-                        <input type="password" class="form-control" placeholder="Contraseña" id="password" name="password" required>
+                        <input type="password" class="form-control" placeholder="Contraseña" id="password" name="password">
                     </div>
                     <div class="form-group">
                         <input type="submit" value="Iniciar" class="btn float-right login_btn">
                     </div>
                 </form>
+            </div>
+            <div class="card-footer">
+                <div class="d-flex justify-content-center links">
+                    Aún no tienes una cuenta?<a href="{{ route('register') }}">Regístrate</a>
+                </div>
             </div>
         </div>
     </div>
@@ -47,6 +62,12 @@
             // Obtén los valores de los campos del formulario
             var email = $('#email').val();
             var password = $('#password').val();
+
+            // Construye el objeto JSON
+            var loginData = {
+                email: email,
+                password: password
+            };
 
             // Envía los datos a tu backend Laravel
             $.ajax({
