01fcc1bcfef9a13c4097f7beb43c75b53957fa6b
Aldair0308 <al222111405@gmail.com>
2024-08-05 18:07:49 -0600

    mesas index

--- resources/views/mesa/index.blade.php
+++ resources/views/mesa/index.blade.php
@@ -1,11 +1,6 @@
 @extends('adminlte::page')
 
-
 @section('title', 'Mesas')
-
-@section('content_header')
-    <h1>Dashboard</h1>
-@stop
 
 @section('content')
 <div class="container-fluid">
@@ -14,16 +9,14 @@
             <div class="card">
                 <div class="card-header">
                     <div style="display: flex; justify-content: space-between; align-items: center;">
-
                         <span id="card_title">
                             {{ __('Mesa') }}
                         </span>
-
-                         <div class="float-right">
+                        <div class="float-right">
                             <a href="{{ route('mesas.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
                               {{ __('Create New') }}
                             </a>
-                          </div>
+                        </div>
                     </div>
                 </div>
                 @if ($message = Session::get('success'))
@@ -34,17 +27,15 @@
 
                 <div class="card-body bg-white">
                     <div class="table-responsive">
-                        <table class="table table-striped table-hover">
+                        <table id="mesas-table" class="table table-striped table-hover">
                             <thead class="thead">
                                 <tr>
                                     <th>No</th>
-                                    
                                     <th>Nomesa</th>
                                     <th>Cliente</th>
                                     <th>Estado</th>
                                     <th>Totalcuenta</th>
                                     <th>Horapago</th>
-
                                     <th></th>
                                 </tr>
                             </thead>
@@ -52,13 +43,11 @@
                                 @foreach ($mesas as $mesa)
                                     <tr>
                                         <td>{{ ++$i }}</td>
-                                        
                                         <td>{{ $mesa->noMesa }}</td>
                                         <td>{{ $mesa->cliente }}</td>
                                         <td>{{ $mesa->estado }}</td>
                                         <td>{{ $mesa->totalCuenta }}</td>
                                         <td>{{ $mesa->horaPago }}</td>
-
                                         <td>
                                             <form action="{{ route('mesas.destroy',$mesa->id) }}" method="POST">
                                                 <a class="btn btn-sm btn-primary " href="{{ route('mesas.show',$mesa->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
@@ -83,8 +72,6 @@
 
 @section('css')
     {{-- Add here extra stylesheets --}}
-    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
-    {{-- Incluir estilos de DataTables --}}
     <link rel="stylesheet" href="//cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="{{ secure_asset('vendor/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('vendor/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
@@ -98,14 +85,12 @@
     <script src="{{ secure_asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ secure_asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
     <script src="{{ secure_asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
-    
-    {{-- Script de DataTables --}}
     <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
     <script src="//cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 
     <script>
         $(document).ready(function() {
-            $('#rondas-table').DataTable({
+            $('#mesas-table').DataTable({
                 "language": {
                     "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json",
                     "info": "_START_ / _END_ de un total de _TOTAL_",
