ad8e4b9c8837753f320adc9d1345278949812f23
Aldairm0308 <al222111405@gmail.com>
2024-12-21 23:28:06 -0600

    Carrito mesa value edioted

--- public/js/carrito.js
+++ public/js/carrito.js
@@ -189,10 +189,11 @@ ordenForm.addEventListener('submit', function (event) {
         descripciones.push(descripcion);
     });
 
-    const mesa = ordenForm.querySelector('#mesa')?.value || 'Invitado';
-    const numeroMesa = parseInt(ordenForm.querySelector('#numeroMesa')?.value || '0');
-    const estado = ordenForm.querySelector('#estado')?.value || 'por_preparar';
-    const mesero = ordenForm.querySelector('#mesero')?.value || 'Invitado';
+    const mesa = "{{ $name }} {{ $lastName }}".trim() || "Invitado";
+    const numeroMesa = parseInt(ordenForm.querySelector('#numeroMesa')?.value || "0");
+    const estado = ordenForm.querySelector('#estado')?.value || "por_preparar";
+    const mesero = ordenForm.querySelector('#mesero')?.value || "Invitado";
+    
 
     const totalRondaInt = parseFloat(totalDisplay?.textContent || '0');
 
