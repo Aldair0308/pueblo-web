473105882c4d473e20d39a0eaef7a247a8d46f57
Aldairm0308 <al222111405@gmail.com>
2024-12-22 18:32:49 -0600

    Ordenar controller edited

--- app/Http/Controllers/OrdenarController.php
+++ app/Http/Controllers/OrdenarController.php
@@ -10,6 +10,7 @@ class OrdenarController extends Controller
     {
         // Obtener los datos del usuario desde la sesión
         $name = session('user.name', 'Invitado');
+        $firstName = session('user.first_name', 'Invitado');
         $lastName = session('user.last_name', '');
         $photo = session('user.photo', '/default-avatar.png');
 
@@ -19,6 +20,7 @@ class OrdenarController extends Controller
             'name' => $name,
             'lastName' => $lastName,
             'photo' => $photo,
+            'firstName' => $firstName,
         ]);
     }
 }
