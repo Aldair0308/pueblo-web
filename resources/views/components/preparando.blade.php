5dcb340d1ef3f41b50d1846558c9fc634e4b4392
Aldairm0308 <al222111405@gmail.com>
2024-12-26 18:51:03 -0600

    Preparando v1 added

--- resources/views/components/preparando.blade.php
+++ resources/views/components/preparando.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center justify-center h-screen">
     <div class="bg-white rounded-lg shadow-md p-6 text-center w-full max-w-md">
         <h2 class="text-lg font-bold text-gray-800">Estamos preparando tu orden...</h2>
-        <p class="text-gray-600 mt-2">Por favor, espera mientras procesamos tu pedido.</p>
+        <p class="text-gray-600 mt-2">Por favor, espera mientras procesamos tu pedido...</p>
     </div>
 </div>
