c96ef77633f01e5332997b9834f3a58cc8568155
Aldairm0308 <al222111405@gmail.com>
2024-12-26 19:02:17 -0600

    Preparando component render 4 sec

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -24,7 +24,28 @@
             <h2>{{ session('user')['first_name'] }} {{ session('user')['last_name'] }}</h2>
         </div>
         <x-Cuenta />
-        <x-preparando />
+
+        <!-- Contenedor para el componente x-preparando -->
+        <div id="preparando-container"></div>
+
+        <script>
+            // Función para agregar el componente x-preparando cada 4 segundos
+            function renderPreparando() {
+                // Obtener el contenedor donde se agregará el componente
+                const container = document.getElementById('preparando-container');
+
+                // Crear un nuevo elemento div para el componente x-preparando
+                const preparacion = document.createElement('div');
+                preparacion.innerHTML =
+                    `@component('x-preparando') @endcomponent`;
+
+                // Agregar el componente al contenedor
+                container.appendChild(preparacion);
+            }
+
+            // Llamar a la función cada 4 segundos
+            setInterval(renderPreparando, 4000);
+        </script>
     @else
         <p>No se encontró información del usuario.</p>
     @endif
